caa8d0ce1441abd1f6143aa4b71d578b0164e2de
Yuria Sato <yuria.sato.sys@gmail.com>
2017-09-11 11:42:06 +0900

    プログラムの修正 #1

--- src/HighOrLow.java
+++ src/HighOrLow.java
@@ -11,32 +11,35 @@ public class HighOrLow {
 
 		Random rnd = new Random();
 		num1 = rnd.nextInt(14) + 1;
-		for(int i = 0; 1 < 5; i++) {
-			num2 = rnd.nextInt(14) + 1;
+		for(int i = 0; i < 5; i++) {
+			num2 = rnd.nextInt(13) + 1;
 			if(num1 == num2) {
 				do {
-					num2 = rnd.nextInt(14) + 1;
+					num2 = rnd.nextInt(13) + 1;
 				}while(num1 != num2);
 			}
 
 			System.out.println("High or Low?   1：High   2：Low");
-			System.out.print(num1 + "   --> ");
+			System.out.print(num1 + "(" +num2 +")" + "   --> ");
 			Scanner sc = new Scanner(System.in);
-			while(ans != 1 || ans != 2) {
-				ans = sc.nextInt();
-			}
+			ans = sc.nextInt();
 			if(num1 < num2 && ans == 1) {
 				System.out.println("正解！");
 				pnt++;
-			}else if(num1 > num2 && ans == 1) {
+				num1 = num2;
+			}
+			if(num1 > num2 && ans == 1) {
 				System.out.println("不正解！");
 				break;
-			}else if(num1 < num2 && ans == 2) {
+			}
+			if(num1 < num2 && ans == 2) {
 				System.out.println("不正解！");
 				break;
-			}else if(num1 < num2 && ans == 2) {
+			}
+			if(num1 > num2 && ans == 2) {
 				System.out.println("正解！");
 				pnt++;
+				num1 = num2;
 			}
 		}
 		if(pnt == 5) {
